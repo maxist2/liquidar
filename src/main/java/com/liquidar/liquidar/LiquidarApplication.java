6b0bb38ce39236a917f11d9d201c944d15fec783
maxist2 <59034778+maxist2@users.noreply.github.com>
2024-04-01 19:27:47 -0500

    Add files via upload

---
+++ src/main/java/com/liquidar/liquidar/LiquidarApplication.java
@@ -0,0 +1,13 @@
+package com.liquidar.liquidar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liquidar/liquidar/LiquidarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquidar.liquidar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
